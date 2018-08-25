6992c07298dfccfcff7cbd3fdbbde15f2b1bba95
soogan <39460732+soogan@users.noreply.github.com>
2018-08-25 16:22:32 +0900

    중화기 장전 스프라이트 추가

--- core/src/main/java/com/gfpixel/gfpixeldungeon/sprites/TowSprite.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/sprites/TowSprite.java
@@ -34,13 +34,13 @@ public class TowSprite extends MobSprite {
 
         texture( Assets.TOW );
 
-        TextureFilm frames = new TextureFilm( texture, 50, 26 );
+        TextureFilm frames = new TextureFilm( texture, 50, 25 );
 
         idle = new Animation( 5, true );
-        idle.frames( frames, 0, 1 );
+        idle.frames( frames, 0, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 0 );
 
         run = new Animation( 10, true );
-        run.frames( frames, 0, 1 );
+        run.frames( frames, 1, 2, 3, 4, 5 );
 
         attack = new Animation( 10, false );
         attack.frames( frames, 2, 2, 2 );
