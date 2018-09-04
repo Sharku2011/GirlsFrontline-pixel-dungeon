4d9748ab6bf6300a34d9a0430d43922133105c73
soogan <39460732+soogan@users.noreply.github.com>
2018-09-04 21:42:08 +0900

    아주 간단한 문제 해결
    이런 단순한거때문에 이틀동안 머리굴림
    빡대가리라 죄송해요

--- core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/Golem.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/Golem.java
@@ -21,218 +21,58 @@
 
 package com.gfpixel.gfpixeldungeon.actors.mobs;
 
-import com.gfpixel.gfpixeldungeon.Dungeon;
-import com.gfpixel.gfpixeldungeon.actors.Actor;
 import com.gfpixel.gfpixeldungeon.actors.Char;
+import com.gfpixel.gfpixeldungeon.actors.buffs.Amok;
+import com.gfpixel.gfpixeldungeon.actors.buffs.Sleep;
 import com.gfpixel.gfpixeldungeon.actors.buffs.Terror;
-import com.gfpixel.gfpixeldungeon.effects.CellEmitter;
-import com.gfpixel.gfpixeldungeon.effects.particles.PurpleParticle;
-import com.gfpixel.gfpixeldungeon.items.wands.WandOfDisintegration;
-import com.gfpixel.gfpixeldungeon.items.weapon.enchantments.Grim;
-import com.gfpixel.gfpixeldungeon.items.weapon.enchantments.Vampiric;
-import com.gfpixel.gfpixeldungeon.mechanics.Ballistica;
-import com.gfpixel.gfpixeldungeon.messages.Messages;
-import com.gfpixel.gfpixeldungeon.scenes.GameScene;
-import com.gfpixel.gfpixeldungeon.sprites.CharSprite;
+import com.gfpixel.gfpixeldungeon.actors.mobs.npcs.Imp;
 import com.gfpixel.gfpixeldungeon.sprites.GolemSprite;
-import com.gfpixel.gfpixeldungeon.utils.GLog;
-import com.watabou.utils.Bundle;
 import com.watabou.utils.Random;
 
 public class Golem extends Mob {
-	
+
 	{
 		spriteClass = GolemSprite.class;
-		
-		HP = HT = 40;
-		defenseSkill = 5;
-		
+
+		HP = HT = 85;
+		defenseSkill = 18;
+
 		EXP = 12;
 		maxLvl = 22;
 
-		properties.add(Property.INORGANIC);
+		properties.add(Property.UNDEAD);
 	}
-
-	public int DamageReducer() { return 3; }
 
 	@Override
 	public int damageRoll() {
-		return Random.NormalIntRange(25, 35);
+		return Random.NormalIntRange( 25, 40 );
 	}
 
 	@Override
 	public int attackSkill( Char target ) {
-		return 30;
+		return 28;
 	}
 
 	@Override
 	protected float attackDelay() {
-		return 0.5f;
+		return 1.5f;
 	}
 
 	@Override
 	public int drRoll() {
-		return Random.NormalIntRange(0, 10);
-	}
-
-	private Ballistica beam;
-	private int beamTarget = -1;
-	private int beamCooldown;
-	public boolean beamCharged;
-
-	@Override
-	protected boolean canAttack( Char enemy ) {
-
-		if (beamCooldown == 0) {
-			Ballistica aim = new Ballistica(pos, enemy.pos, Ballistica.STOP_TERRAIN);
-
-			if (enemy.invisible == 0 && !isCharmedBy(enemy) && fieldOfView[enemy.pos] && aim.subPath(1, aim.dist).contains(enemy.pos)){
-				beam = aim;
-				beamTarget = aim.collisionPos;
-				return true;
-			} else
-				//if the beam is charged, it has to attack, will aim at previous location of target.
-				return beamCharged;
-		} else
-			return super.canAttack(enemy);
+		return Random.NormalIntRange(0, 12);
 	}
 
 	@Override
-	protected boolean act() {
-		if (beamCharged && state != HUNTING){
-			beamCharged = false;
-		}
-		if (beam == null && beamTarget != -1) {
-			beam = new Ballistica(pos, beamTarget, Ballistica.STOP_TERRAIN);
-			sprite.turnTo(pos, beamTarget);
-		}
-		if (beamCooldown > 0)
-			beamCooldown--;
-		return super.act();
-	}
+	public void die( Object cause ) {
+		Imp.Quest.process( this );
 
-	@Override
-	protected boolean doAttack( Char enemy ) {
-
-		if (beamCooldown > 0) {
-			return super.doAttack(enemy);
-		} else if (!beamCharged){
-			((GolemSprite)sprite).charge( enemy.pos );
-			spend( attackDelay()*2f );
-			beamCharged = true;
-			return true;
-		} else {
-
-			spend( attackDelay() );
-
-			beam = new Ballistica(pos, beamTarget, Ballistica.STOP_TERRAIN);
-			if (Dungeon.level.heroFOV[pos] || Dungeon.level.heroFOV[beam.collisionPos] ) {
-				sprite.zap( beam.collisionPos );
-				return false;
-			} else {
-				deathGaze();
-				return true;
-			}
-		}
-
-	}
-
-	// Reduce damage during charge. Nerf this 4 to 2.
-	@Override
-	public void damage(int dmg, Object src) {
-		if (beamCharged) dmg /= DamageReducer();
-		super.damage(dmg, src);
-	}
-
-	public void deathGaze(){
-		if (!beamCharged || beamCooldown > 0 || beam == null)
-			return;
-
-		beamCharged = false;
-		beamCooldown = Random.IntRange(3, 6);
-
-		boolean terrainAffected = false;
-
-		for (int pos : beam.subPath(1, beam.dist)) {
-
-			if (Dungeon.level.flamable[pos]) {
-
-				Dungeon.level.destroy( pos );
-				GameScene.updateMap( pos );
-				terrainAffected = true;
-
-			}
-
-			Char ch = Actor.findChar( pos );
-			if (ch == null) {
-				continue;
-			}
-
-			if (hit( this, ch, true )) {
-				ch.damage( Random.NormalIntRange( 30, 50 ), this );
-
-				if (Dungeon.level.heroFOV[pos]) {
-					ch.sprite.flash();
-					CellEmitter.center( pos ).burst( PurpleParticle.BURST, Random.IntRange( 1, 2 ) );
-				}
-
-				if (!ch.isAlive() && ch == Dungeon.hero) {
-					Dungeon.fail( getClass() );
-					GLog.n( Messages.get(this, "deathgaze_kill") );
-				}
-			} else {
-				ch.sprite.showStatus( CharSprite.NEUTRAL,  ch.defenseVerb() );
-			}
-		}
-
-		if (terrainAffected) {
-			Dungeon.observe();
-		}
-
-		beam = null;
-		beamTarget = -1;
-	}
-
-	private static final String BEAM_TARGET     = "beamTarget";
-	private static final String BEAM_COOLDOWN   = "beamCooldown";
-	private static final String BEAM_CHARGED    = "beamCharged";
-
-	@Override
-	public void storeInBundle(Bundle bundle) {
-		super.storeInBundle(bundle);
-		bundle.put( BEAM_TARGET, beamTarget);
-		bundle.put( BEAM_COOLDOWN, beamCooldown );
-		bundle.put( BEAM_CHARGED, beamCharged );
-	}
-
-	@Override
-	public void restoreFromBundle(Bundle bundle) {
-		super.restoreFromBundle(bundle);
-		if (bundle.contains(BEAM_TARGET))
-			beamTarget = bundle.getInt(BEAM_TARGET);
-		beamCooldown = bundle.getInt(BEAM_COOLDOWN);
-		beamCharged = bundle.getBoolean(BEAM_CHARGED);
+		super.die( cause );
 	}
 
 	{
-		resistances.add( WandOfDisintegration.class );
-		resistances.add( Grim.class );
-		resistances.add( Vampiric.class );
-	}
-
-	{
+		immunities.add( Amok.class );
 		immunities.add( Terror.class );
-	}
-
-	private class Hunting extends Mob.Hunting{
-		@Override
-		public boolean act(boolean enemyInFOV, boolean justAlerted) {
-			//even if enemy isn't seen, attack them if the beam is charged
-			if (beamCharged && enemy != null && canAttack(enemy)) {
-				enemySeen = enemyInFOV;
-				return doAttack(enemy);
-			}
-			return super.act(enemyInFOV, justAlerted);
-		}
+		immunities.add( Sleep.class );
 	}
 }
--- core/src/main/java/com/gfpixel/gfpixeldungeon/sprites/GolemSprite.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/sprites/GolemSprite.java
@@ -22,99 +22,43 @@
 package com.gfpixel.gfpixeldungeon.sprites;
 
 import com.gfpixel.gfpixeldungeon.Assets;
-import com.gfpixel.gfpixeldungeon.actors.Actor;
-import com.gfpixel.gfpixeldungeon.actors.Char;
-import com.gfpixel.gfpixeldungeon.actors.mobs.Jupiter;
-import com.gfpixel.gfpixeldungeon.effects.Beam;
-import com.gfpixel.gfpixeldungeon.effects.MagicMissile;
 import com.gfpixel.gfpixeldungeon.effects.particles.ElmoParticle;
-import com.gfpixel.gfpixeldungeon.tiles.DungeonTilemap;
 import com.watabou.noosa.TextureFilm;
-import com.watabou.noosa.particles.Emitter;
 
 public class GolemSprite extends MobSprite {
 
-	private int zapPos;
-
-	private Animation charging;
-	private Emitter chargeParticles;
-
 	public GolemSprite() {
 		super();
-		
+
 		texture( Assets.GOLEM );
-		
+
 		TextureFilm frames = new TextureFilm( texture, 16, 16 );
-		
+
 		idle = new Animation( 4, true );
 		idle.frames( frames, 0, 1 );
 
-		charging = new Animation( 12, true);
-		charging.frames( frames, 6 );
-
-		chargeParticles = centerEmitter();
-		chargeParticles.autoKill = false;
-		chargeParticles.pour(MagicMissile.MagicParticle.ATTRACTING, 0.05f);
-		chargeParticles.on = false;
-
 		run = new Animation( 12, true );
 		run.frames( frames, 2, 3, 4, 5 );
-		
-		attack = new Animation( 10, false );
-		attack.frames( frames,  7, 8, 9 );
-		zap = attack.clone();
 
-		die = new Animation( 8, false );
-		die.frames( frames, 10, 11, 12, 13 );
+		attack = new Animation( 10, false );
+		attack.frames( frames, 6, 7, 8 );
+
+		die = new Animation( 15, false );
+		die.frames( frames, 9, 10, 11, 12, 13 );
 
 		play( idle );
 	}
 
 	@Override
-	public void link(Char ch) {
-		super.link(ch);
-		if (((Jupiter)ch).beamCharged) play(charging);
-	}
-
-	@Override
-	public void update() {
-		super.update();
-		chargeParticles.pos(center());
-		chargeParticles.visible = visible;
-	}
-
-	public void charge( int pos ){
-		turnTo(ch.pos, pos);
-		play(charging);
-	}
-
-	@Override
-	public void play(Animation anim) {
-		chargeParticles.on = anim == charging;
-		super.play(anim);
-	}
-
-	@Override
-	public void zap( int pos ) {
-		zapPos = pos;
-		super.zap( pos );
+	public int blood() {
+		return 0xFF80706c;
 	}
 
 	@Override
 	public void onComplete( Animation anim ) {
-		super.onComplete( anim );
-
-		if (anim == zap) {
-			idle();
-			if (Actor.findChar(zapPos) != null){
-				parent.add(new Beam.DeathRay(center(), Actor.findChar(zapPos).sprite.center()));
-			} else {
-				parent.add(new Beam.DeathRay(center(), DungeonTilemap.raisedTileCenterToWorld(zapPos)));
-			}
-			((Jupiter)ch).deathGaze();
-			ch.next();
-		} else if (anim == die){
-			chargeParticles.killAndErase();
+		if (anim == die) {
+			emitter().burst( ElmoParticle.FACTORY, 4 );
 		}
+		super.onComplete( anim );
 	}
 }
