42bd6d25917dcb8fd1af70baebe7d86415664998
Sharku2011 <pjsw007@naver.com>
2018-10-03 01:10:21 +0900

    2테마용 약화된 야거 추가
    기존 2테마 야거(shaman)는 헌팅래빗에서만 제한적으로 교체

--- core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/jager.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/jager.java
@@ -1,4 +1,22 @@
 package com.gfpixel.gfpixeldungeon.actors.mobs;
 
-public class jager {
+import com.watabou.utils.Random;
+
+public class Jager extends Warlock {
+    {
+
+        HP = HT = 20;
+        defenseSkill = 8;
+
+        EXP = 71;
+        maxLvl = 11;
+
+
+        properties.add(Property.UNDEAD);
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange( 6, 12 );
+    }
 }
