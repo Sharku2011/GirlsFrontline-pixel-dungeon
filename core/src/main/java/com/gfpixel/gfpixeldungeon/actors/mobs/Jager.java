ede2180cded3b259bab27217881431a1b99bc053
Sharku2011 <pjsw007@naver.com>
2018-10-13 03:45:32 +0900

    Update core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/Jager.java

---
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/Jager.java
@@ -0,0 +1,23 @@
+package com.gfpixel.gfpixeldungeon.actors.mobs;
+
+import com.watabou.utils.Random;
+
+public class Jager extends Warlock {
+    {
+        HP = HT = 20;
+        defenseSkill = 8;
+
+        EXP = 6;
+        maxLvl = 14;
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange( 3, 7 );
+    }
+
+    @Override
+    public int drRoll() {
+        return Random.NormalIntRange(1, 2);
+    }
+}
