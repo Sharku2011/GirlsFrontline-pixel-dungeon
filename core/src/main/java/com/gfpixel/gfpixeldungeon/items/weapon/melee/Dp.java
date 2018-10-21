65034500d6223e0e3598bf783dfd797920f6eabf
Sharku2011 <pjsw007@naver.com>
2018-10-22 01:18:27 +0900

    기관총 류 기습 데미지 하향
    기관총 류의 최소 데미지가 1턴당 때릴 수 있는 타수로 나눈 만큼만 보장되도록 변경
    디버깅 킷 변경(스프링필드 제거, 기관총류 추가)

--- core/src/main/java/com/gfpixel/gfpixeldungeon/items/weapon/melee/Dp.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/items/weapon/melee/Dp.java
@@ -50,10 +50,9 @@ public class Dp extends MeleeWeapon {
             Char enemy = hero.enemy();
             if (enemy instanceof Mob && ((Mob) enemy).surprisedBy(hero)) {
                 //deals 85% toward max to max on surprise, instead of min to max.
-                int diff = max() - min();
                 int damage = augment.damageFactor(Random.NormalIntRange(
-                        min() + Math.round(diff*0.4f),
-                        max()));
+                        Math.max(1, Math.round(min()*DLY)),
+                        Math.round(max()*DLY)));
                 int exStr = hero.STR() - STRReq();
                 if (exStr > 0) {
                     damage -= Random.IntRange(0, exStr);
