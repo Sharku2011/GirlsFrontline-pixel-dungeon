f649ab7f054206e4531d44e9385771b7ba29031b
Sharku2011 <pjsw007@naver.com>
2018-12-31 03:23:19 +0900

    임시 무기 분류 추가
    개인 방어화기

---
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/items/weapon/melee/PDW/PersonalDefenseWeapon.java
@@ -0,0 +1,9 @@
+package com.gfpixel.gfpixeldungeon.items.weapon.melee.PDW;
+
+import com.gfpixel.gfpixeldungeon.items.weapon.melee.MeleeWeapon;
+
+public abstract class PersonalDefenseWeapon extends MeleeWeapon {
+
+    // 임시로 추가한 분류
+    // SA 계통의 무기와 AR계통의 무기중 일부를 옮길수도
+}
