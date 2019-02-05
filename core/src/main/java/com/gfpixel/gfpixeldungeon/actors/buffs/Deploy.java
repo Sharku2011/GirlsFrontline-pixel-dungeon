1a5fefd87dcf5f6b9125191d621705b37106a306
Sharku2011 <pjsw007@naver.com>
2019-02-06 05:28:19 +0900

    Revert "Revert "4티어 방어구 장착 시 크래시나던 이슈 해결""
    This reverts commit bfcd451c5f57dec61156f30963faa2e2acdc9db0.

--- core/src/main/java/com/gfpixel/gfpixeldungeon/actors/buffs/Deploy.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/actors/buffs/Deploy.java
@@ -1,9 +1,35 @@
 package com.gfpixel.gfpixeldungeon.actors.buffs;
 
-public class Deploy extends Roots {
+import com.gfpixel.gfpixeldungeon.actors.Char;
+
+public class Deploy extends FlavourBuff {
 
     {
         type = buffType.NEGATIVE;
+    }
+
+    // 이렇게 하지 말고 한 자리에서 가만히 있으면 스택이 쌓이는 버프로 만들어서 deploying 건드려야 할듯
+    @Override
+    public boolean attachTo( Char target ) {
+        if (super.attachTo( target )) {
+            if (!target.flying)
+            {
+                target.rooted = true;
+            }
+            target.deploying = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void detach() {
+        target.deploying = false;
+        if (target.buffs(Roots.class).isEmpty()) {
+            target.rooted = false;
+        }
+        super.detach();
     }
 
 }
