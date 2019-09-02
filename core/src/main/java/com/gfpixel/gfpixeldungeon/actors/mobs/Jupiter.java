089437f12b23b854f5bbc83da0bf56d7f626f9ad
Sharku2011 <pjsw007@naver.com>
2019-09-03 04:50:51 +0900

    [WIP] 간헐적으로 빔 형태의 공격이 이상한 방향으로 가해지는 버그
    누락된 커밋 추가

--- core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/Jupiter.java
+++ core/src/main/java/com/gfpixel/gfpixeldungeon/actors/mobs/Jupiter.java
@@ -90,6 +90,9 @@ public class Jupiter extends Mob {
 
             if (enemy.invisible == 0 && !isCharmedBy(enemy) && fieldOfView[enemy.pos] && aim.subPath(1, aim.dist).contains(enemy.pos)){
                 beam = aim;
+                if (aim.collisionPos == -1) {
+                    return false;
+                }
                 beamTarget = aim.collisionPos;
                 return true;
             } else
